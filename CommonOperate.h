c11e12223e26d0c896756a49fecb91b43ced87cb
薛皓天 <123456>
2024-09-09 16:58:58 +0800

    更新Basic Rtype

---
+++ CommonOperate.h
@@ -0,0 +1,58 @@
+#pragma once
+#ifndef CommonOperate
+
+#include<iostream>
+#include<vector>
+#include<cstring>
+#include<map>
+#include<unordered_map>
+
+using namespace std;
+
+// 32位机 int即可
+int reg[32] = {0}; // RISC-V 32个通用寄存器
+unordered_map<string , int> reg_map; // 用于寄存器名到寄存器编号的映射
+int PC; // 程序计数器
+
+// 初始化寄存器
+void init_reg()
+{
+    // 用于寄存器名到寄存器编号的映射
+    // 解析指令时使用
+    reg_map["zero"]= 0;
+    reg_map ["ra"] = 1;
+    reg_map ["sp"] = 2; 
+    reg_map ["gp"] = 3;
+    reg_map ["tp"] = 4;
+    reg_map ["t0"] = 5;
+    reg_map ["t1"] = 6;
+    reg_map ["t2"] = 7;
+    reg_map ["s0"] = 8;
+    reg_map ["s1"] = 9;
+    reg_map ["a0"] = 10;
+    reg_map ["a1"] = 11;
+    reg_map ["a2"] = 12;
+    reg_map ["a3"] = 13;
+    reg_map ["a4"] = 14;
+    reg_map ["a5"] = 15;
+    reg_map ["a6"] = 16;
+    reg_map ["a7"] = 17;
+    reg_map ["s2"] = 18;
+    reg_map ["s3"] = 19;
+    reg_map ["s4"] = 20;
+    reg_map ["s5"] = 21;
+    reg_map ["s6"] = 22;
+    reg_map ["s7"] = 23;
+    reg_map ["s8"] = 24;
+    reg_map ["s9"] = 25;
+    reg_map ["s10"] = 26;
+    reg_map ["s11"] = 27;
+    reg_map ["t3"] = 28;
+    reg_map ["t4"] = 29;
+    reg_map ["t5"] = 30;
+    reg_map ["t6"] = 31;
+}
+
+
+
+#endif
--- Rtype.cpp
+++ Rtype.cpp
@@ -3,6 +3,8 @@
 
 #include<iostream>
 
+#include "CommonOperate.h"
+
 using namespace std;
 
 class Rtype
@@ -12,17 +14,80 @@ class Rtype
         一共有9条
     */
     public:
-      static void ADD(int rd, int rs1, int rs2);
-      static void SUB(int rd, int rs1, int rs2);
-      static void XOR(int rd, int rs1, int rs2);
-      static void SRL(int rd, int rs1, int rs2);
-      static void OR(int rd, int rs1, int rs2);
-      static void AND(int rd, int rs1, int rs2);
-      static void SLL(int rd, int rs1, int rs2);
-      static void SLT(int rd, int rs1, int rs2);
-      static void SRA(int rd, int rs1, int rs2); 
+      static void ADD(int rd, int rs1, int rs2); // 32位加法
+      static void SUB(int rd, int rs1, int rs2); // 32位减法
+      static void XOR(int rd, int rs1, int rs2); // 逻辑异或
+      static void SRL(int rd, int rs1, int rs2); // 逻辑右移
+      static void OR(int rd, int rs1, int rs2); // 逻辑或
+      static void AND(int rd, int rs1, int rs2); // 逻辑与
+      static void SLL(int rd, int rs1, int rs2); // 逻辑左移
+      static void SLT(int rd, int rs1, int rs2); // 比较
+      static void SRA(int rd, int rs1, int rs2); // 逻辑右移
 };
 
+void Rtype::ADD(int rd, int rs1, int rs2)
+{
+    reg[rd] = reg[rs1] + reg[rs2];
+    PC += 4; // 32位按字节编址，指令长度为4
+}
+
+void Rtype::SUB(int rd, int rs1, int rs2)
+{
+    reg[rd] = reg[rs1] - reg[rs2];
+    PC += 4;
+}
+
+void Rtype::XOR(int rd, int rs1, int rs2)
+{
+    reg[rd] = reg[rs1] ^ reg[rs2];
+    PC += 4;
+}
+
+void Rtype::SRL(int rd, int rs1, int rs2)
+{
+    // 逻辑右移，将数据作为无符号数进行右移
+    reg[rd] = (unsigned int)reg[rs1] >> reg[rs2];
+    PC += 4;
+}
+
+void Rtype::OR(int rd, int rs1, int rs2)
+{
+    reg[rd] = reg[rs1] | reg[rs2];
+    PC += 4;
+}
+
+void Rtype::AND(int rd, int rs1, int rs2)
+{
+    reg[rd] = reg[rs1] & reg[rs2];
+    PC += 4;
+}
+
+void Rtype::SLL(int rd, int rs1, int rs2)
+{
+    // 逻辑左移，将数据作为无符号数进行左移
+    reg[rd] = (unsigned int)reg[rs1] << reg[rs2];
+    PC += 4;
+}
+
+void Rtype::SLT(int rd, int rs1, int rs2)
+{
+    if (reg[rs1] < reg[rs2])
+    {
+        reg[rd] = 1;
+    }
+    else
+    {
+        reg[rd] = 0;
+    }
+    PC += 4;
+}
+
+void Rtype::SRA(int rd, int rs1, int rs2)
+{
+    // 右移算术逻辑运算
+    reg[rd] = reg[rs1] >> reg[rs2];
+    PC += 4;
+}
 
 
 #endif
